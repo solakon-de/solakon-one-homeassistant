545b71a673789074ce4e0665b046ed677bad94b4
Christopher Fenner <christopher.fenner@me.com>
2025-12-10 23:51:11 +0100

    use bitflags

--- custom_components/solakon_one/binary_sensor.py
+++ custom_components/solakon_one/binary_sensor.py
@@ -1,10 +1,13 @@
-"""Sensor platform for Solakon ONE integration."""
+"""Binary sensor platform for Solakon ONE integration."""
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass
 import logging
 
+from bitflags import BitFlags
+
 from homeassistant.components.binary_sensor import (
-#    BinarySensorDeviceClass,
     BinarySensorEntity,
     BinarySensorEntityDescription,
 )
@@ -18,12 +21,24 @@ from .entity import SolakonEntity
 
 _LOGGER = logging.getLogger(__name__)
 
+
+class Status3(BitFlags):
+    options = {
+        0: "island_mode",
+    }
+
+@dataclass(frozen=True)
+class SolakonBinarySensorEntityDescription(BinarySensorEntityDescription):
+    """Describes Solakon ONE binary sensor entity."""
+
+    value_fn: Callable[[dict], bool] | None = None
+
 # Binary sensor entity descriptions for Home Assistant
-BINARY_SENSOR_ENTITY_DESCRIPTIONS: tuple[BinarySensorEntityDescription, ...] = (
-    BinarySensorEntityDescription(
+BINARY_SENSOR_ENTITY_DESCRIPTIONS: tuple[SolakonBinarySensorEntityDescription, ...] = (
+    SolakonBinarySensorEntityDescription(
         key="island_mode",
-        #device_class=BinarySensorDeviceClass.SMOKE,
         entity_category=EntityCategory.DIAGNOSTIC,
+        value_fn=lambda data: Status3(data.get("status_3", 0))["island_mode"] == 1,
     )
 )
 
@@ -38,17 +53,18 @@ async def async_setup_entry(
 
     # Get device info for all binary sensors
     device_info = await hub.async_get_device_info()
-    
+
     entities = []
 
-    entities.extend(
+#    entities.extend(
+    entities.append(
         SolakonSensor(
             coordinator,
             config_entry,
             device_info,
-            description,
+            BINARY_SENSOR_ENTITY_DESCRIPTIONS,
         )
-        for description in BINARY_SENSOR_ENTITY_DESCRIPTIONS
+#        for description in BINARY_SENSOR_ENTITY_DESCRIPTIONS
     )
 
     async_add_entities(entities, True)
@@ -66,23 +82,24 @@ class SolakonSensor(SolakonEntity, BinarySensorEntity):
     ) -> None:
         """Initialize the binary sensor."""
         super().__init__(coordinator, config_entry, device_info, description.key)
-        self._sensor_key = description.key
-
+        # Set entity description
         self.entity_description = description
-
         # Set entity ID
         self.entity_id = f"binary_sensor.solakon_one_{description.key}"
-
 
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
 
-        # to be defined
+        if self.coordinator.data and self.entity_description.value_fn:
+            value = self.entity_description.value_fn(self.coordinator.data)
+            self._attr_is_on = bool(value) if isinstance(value, bool) else None
+        else:
+            self._attr_is_on = None
 
         self.async_write_ha_state()
 
     @property
     def available(self) -> bool:
         """Return if entity is available."""
-        return self.coordinator.last_update_success and self._attr_native_value is not None
+        return self.coordinator.last_update_success and self._attr_is_on is not None
