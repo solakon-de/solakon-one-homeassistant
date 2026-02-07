6536d119a6b56179f29e94a858da054a558ec951
codecepts <git@codecepts.de>
2026-02-08 00:34:13 +0100

    fix dictionary union for merging configuration data with options

--- custom_components/solakon_one/__init__.py
+++ custom_components/solakon_one/__init__.py
@@ -17,7 +17,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: SolakonConfigEntry) -> bool:
     """Set up Solakon ONE from a config entry."""
-    hub = get_modbus_hub(hass, entry.options | entry.data)
+    hub = get_modbus_hub(hass, entry.data | entry.options)  # let options override data
 
     try:
         await hub.async_setup()
