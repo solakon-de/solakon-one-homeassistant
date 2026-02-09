24e8d1468dd70e3568ac5c54b1260062c992d823
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2026-02-09 12:13:00 +0100

    Clean up const.py by removing unused code
    Removed unused class CannotConnect and import HomeAssistantError.

--- custom_components/solakon_one/const.py
+++ custom_components/solakon_one/const.py
@@ -3,7 +3,6 @@
 from typing import Final
 
 from homeassistant.const import Platform
-from homeassistant.exceptions import HomeAssistantError
 
 DOMAIN: Final = "solakon_one"
 
@@ -142,7 +141,3 @@ REGISTERS = {
     "network_status": {"address": 49240, "count": 1, "type": "u16", "scale": 1},
 }
 # fmt: on
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
